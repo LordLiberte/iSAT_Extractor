ce2c1642176e9aacc670638cead33caf175eac32
Lord Liberté <GhostRg110@gmail.com>
2024-12-18 21:46:45 +0100

    Eliminar archivos innecesarios

--- Programa/Final_Project_iSAT/Pestana_3.py
+++ Programa/Final_Project_iSAT/Pestana_3.py
@@ -25,57 +25,14 @@ def pestana3(notebook):
             combo_opciones.set(nuevas_columnas[0] if nuevas_columnas else "")
             combo_opciones2["values"] = nuevas_columnas
             combo_opciones2.set(nuevas_columnas[0] if nuevas_columnas else "")
+            diferenciar["values"] = nuevas_columnas
+            diferenciar.set(nuevas_columnas[0] if nuevas_columnas else "")
             últimas_columnas = nuevas_columnas
         
         # Ejecutar nuevamente después de 1 segundo
         pestana3.after(1000, actualizar_combobox)
-
-
-    # Creación de pestaña y botones =======================================================
-    # Pestaña -----------------------------------------------------------------------------
-    pestana3 = ttk.Frame(notebook, width=1280, height=720)
-    pestana3.pack_propagate(False)  # Evita que la pestaña cambie de tamaño
-
-    # Actualizar fecha --------------------------------------------------------------------
-    funciones_generales.actualizar_fecha(pestana3)
-
-    # Desplegable con opciones 1 -----------------------------------------------------------
-    combo_opciones = ttk.Combobox(pestana3, values=list(dataframe_global.columns), width=50)
-    combo_opciones.place(x=50, y=50)
-    eje_x_label = ttk.Label(pestana3, text="Eje X")
-    eje_x_label.place(x=50, y=20, width=40, height=30)
-    
-    # Desplegable con opciones 1 -----------------------------------------------------------
-    combo_opciones2 = ttk.Combobox(pestana3, values=list(dataframe_global.columns), width=50)
-    combo_opciones2.place(x=50, y=110)
-    eje_y_label = ttk.Label(pestana3, text="Eje Y")
-    eje_y_label.place(x=50, y=80, width=40, height=30)
-    
-    # Iniciar la actualización dinámica del Combobox -------------------------------------
-    actualizar_combobox()
-    
-    # Checkbuttons -----------------------------------------------------------------------
-    # Checkbutton Histograma  @@@@
-    histograma_value = tk.BooleanVar()  # Valor que tomará TRUE o FALSE
-    histograma_check = tk.Checkbutton(pestana3, text="Histograma", variable=histograma_value)  # Checkbutton que modofica el valor 
-    histograma_check.place(x=100, y=140, width=80, height=30) # Posición del checkbutton
-    
-    # Checkbutton Dispersión  @@@@
-    dispersion_value = tk.BooleanVar() # Valor que tomará TRUE o FALSE
-    dispersion_check = tk.Checkbutton(pestana3, text="Dispersión", variable=dispersion_value) # Checkbutton que modofica el valor
-    dispersion_check.place(x=96, y=180, width=80, height=30) # Posición del checkbutton
-    
-    # Checkbutton Barras  @@@@
-    barras_value = tk.BooleanVar() # Valor que tomará TRUE o FALSE
-    barras_check = tk.Checkbutton(pestana3, text="Barras", variable=barras_value) # Checkbutton que modofica el valor
-    barras_check.place(x=85, y=220, width=80, height=30) # Posición del checkbutton
-    
-    dict_graficos = {
-        "Gráfico": ["Histograma", "Dispersión", "Barras"],
-        "Value": [histograma_value, dispersion_value, barras_value]
-    }
-    
-    # Función para mostrar el estado de los Checkbuttons @@@
+        
+        # Función para mostrar el estado de los Checkbuttons @@@
     def mostrar_estado():
         # Comprueba que solo se seleccione uno
         if (
@@ -122,9 +79,92 @@ def pestana3(notebook):
             else:
                 messagebox.showinfo("Info", f"Has seleccionado realizar un gráfico de barras de {combo_opciones.get()} -- {combo_opciones2.get()}")
 
-    # Botón para ejecutar la función mostrar_estado ------------------------------------
+    def realizar_grafico():
+        
+        global dataframe_global
+        if dataframe_global.empty:
+            dataframe_global = Pestana_2.obtener_dataframe()  # Obtenemos Dataframe de Pestaña 2
+        
+        # Histograma @@@@
+        if histograma_value.get():
+            sns.histplot(data = dataframe_global.sort_values(by=combo_opciones.get()),
+                         x=combo_opciones.get(),
+                         hue="Screwing Status")
+            plt.show()
+        
+        # Dispersión @@@@
+        elif dispersion_value.get():
+            sns.relplot(data=dataframe_global,
+                            x=combo_opciones.get(),
+                            y=combo_opciones2.get())
+            plt.ylim(bottom=0)  # Ajusta la parte inferior del eje Y
+            plt.show()
+            
+        
+        # Barras @@@@
+        elif barras_value.get():
+            sns.barplot(data=dataframe_global.sort_values(by=[combo_opciones.get(), combo_opciones2.get()]),
+                        x=combo_opciones.get(),
+                        y=combo_opciones2.get())
+            plt.show()
+            
+            
+
+
+    # Creación de pestaña y botones =======================================================
+    # Pestaña -----------------------------------------------------------------------------
+    pestana3 = ttk.Frame(notebook, width=1280, height=720)
+    pestana3.pack_propagate(False)  # Evita que la pestaña cambie de tamaño
+
+    # Actualizar fecha --------------------------------------------------------------------
+    funciones_generales.actualizar_fecha(pestana3)
+
+    # Desplegable con opciones 1 -----------------------------------------------------------
+    combo_opciones = ttk.Combobox(pestana3, values=list(dataframe_global.columns), width=50)
+    combo_opciones.place(x=50, y=50)
+    eje_x_label = ttk.Label(pestana3, text="Eje X")
+    eje_x_label.place(x=50, y=20, width=40, height=30)
+    
+    # Desplegable con opciones 1 -----------------------------------------------------------
+    combo_opciones2 = ttk.Combobox(pestana3, values=list(dataframe_global.columns), width=50)
+    combo_opciones2.place(x=50, y=110)
+    eje_y_label = ttk.Label(pestana3, text="Eje Y")
+    eje_y_label.place(x=50, y=80, width=40, height=30)
+    
+    # Iniciar la actualización dinámica del Combobox -------------------------------------
+    actualizar_combobox()
+    
+    # Checkbuttons -----------------------------------------------------------------------
+    # Checkbutton Histograma  @@@@
+    histograma_value = tk.BooleanVar()  # Valor que tomará TRUE o FALSE
+    histograma_check = tk.Checkbutton(pestana3, text="Histograma", variable=histograma_value)  # Checkbutton que modofica el valor 
+    histograma_check.place(x=100, y=140, width=80, height=30) # Posición del checkbutton
+    
+    # Checkbutton Dispersión  @@@@
+    dispersion_value = tk.BooleanVar() # Valor que tomará TRUE o FALSE
+    dispersion_check = tk.Checkbutton(pestana3, text="Dispersión", variable=dispersion_value) # Checkbutton que modofica el valor
+    dispersion_check.place(x=96, y=180, width=80, height=30) # Posición del checkbutton
+    
+    # Checkbutton Barras  @@@@
+    barras_value = tk.BooleanVar() # Valor que tomará TRUE o FALSE
+    barras_check = tk.Checkbutton(pestana3, text="Barras", variable=barras_value) # Checkbutton que modofica el valor
+    barras_check.place(x=85, y=220, width=80, height=30) # Posición del checkbutton
+    
+    # Botón para ejecutar la función mostrar_estado --------------------------------------
     boton_seleccion = tk.Button(pestana3, text="Mostrar Estado", command=mostrar_estado)
     boton_seleccion.place(x=200, y=180)
     
-
+    # Botón para realizar gráfico en función de lo seleccionado ---------------------------
+    boton_graficar = tk.Button(pestana3, text="Realizar Gráfico", command=realizar_grafico)
+    boton_graficar.place(x=400, y=78)
+    
+    # PENDIENTE DE REVISAR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Introducir diferenciación -----------------------------------------------------------
+    diferenciar = ttk.Combobox(pestana3, values=list(dataframe_global.columns), width=50)
+    diferenciar.place(x=700, y=600)
+    hue_label = tk.Label(pestana3, text="Diferenciación")
+    hue_label.place(x=600, y=500)
+    
+    
+    # FIN -------------------------------------------------------------------------------------------
     return pestana3
